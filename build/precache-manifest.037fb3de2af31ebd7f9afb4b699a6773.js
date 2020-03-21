440cbfba741257bf484a2498a42565789858e313
Apelsin16 <apelsin@i.ua>
2020-03-21 18:21:00 +0200

    build.

---
+++ build/precache-manifest.037fb3de2af31ebd7f9afb4b699a6773.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7257b0b85b910e2253cbaf606dfe88",
+    "url": "/test/build/index.html"
+  },
+  {
+    "revision": "422cb00ec1acc91f81c8",
+    "url": "/test/build/static/css/2.6b00acf2.chunk.css"
+  },
+  {
+    "revision": "86e3df8868d99555b4e0",
+    "url": "/test/build/static/css/main.0fb9ca8c.chunk.css"
+  },
+  {
+    "revision": "422cb00ec1acc91f81c8",
+    "url": "/test/build/static/js/2.70f56834.chunk.js"
+  },
+  {
+    "revision": "b90250289e2dd8c0f730bb6af8ce5d4b",
+    "url": "/test/build/static/js/2.70f56834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e3df8868d99555b4e0",
+    "url": "/test/build/static/js/main.23ee0178.chunk.js"
+  },
+  {
+    "revision": "f4c094b39cdd4ad4b652",
+    "url": "/test/build/static/js/runtime-main.775cd093.js"
+  },
+  {
+    "revision": "c2beb09bf0deabda5ca8a9bb475b1df8",
+    "url": "/test/build/static/media/banner.c2beb09b.png"
+  },
+  {
+    "revision": "e1e1779d8df3bbc0961d52d2b8708077",
+    "url": "/test/build/static/media/img.e1e1779d.png"
+  },
+  {
+    "revision": "0ae123c07f5af7f648237d0833a7ba3d",
+    "url": "/test/build/static/media/img1.0ae123c0.png"
+  },
+  {
+    "revision": "a09fbe40b19a274e71bdaaf2d11f4322",
+    "url": "/test/build/static/media/img1.a09fbe40.png"
+  },
+  {
+    "revision": "cc89060538a71dc3be48e93dbe826dc1",
+    "url": "/test/build/static/media/img2.cc890605.png"
+  },
+  {
+    "revision": "514eee293913c8fe60f644b19f50705a",
+    "url": "/test/build/static/media/img3.514eee29.png"
+  },
+  {
+    "revision": "a73dccc4a8a19da4dede7f2abd177d69",
+    "url": "/test/build/static/media/img4.a73dccc4.png"
+  },
+  {
+    "revision": "d7fa38308f46795d73e0ddbb33535ae1",
+    "url": "/test/build/static/media/img5.d7fa3830.png"
+  },
+  {
+    "revision": "83b42f0ab470cd234dfeddbde81817ef",
+    "url": "/test/build/static/media/img6.83b42f0a.png"
+  },
+  {
+    "revision": "c76826a5ec4a82cd613bd3d99483334c",
+    "url": "/test/build/static/media/img7.c76826a5.png"
+  },
+  {
+    "revision": "de5be0609d172a3750029f336254e885",
+    "url": "/test/build/static/media/img8.de5be060.png"
+  },
+  {
+    "revision": "04271724939772097857fb3ec71a4e96",
+    "url": "/test/build/static/media/ralewaylight.04271724.ttf"
+  },
+  {
+    "revision": "2dcb032a6dd7da64da9d8ec4a9ec20f3",
+    "url": "/test/build/static/media/ralewaylight.2dcb032a.woff2"
+  },
+  {
+    "revision": "5f317be0ec3be5f47245b99034212c11",
+    "url": "/test/build/static/media/ralewaylight.5f317be0.woff"
+  },
+  {
+    "revision": "0320ed0967adc2dbb1110d16daf3c029",
+    "url": "/test/build/static/media/ralewaymedium.0320ed09.woff"
+  },
+  {
+    "revision": "272b676b5174b1be5d64805094c688fd",
+    "url": "/test/build/static/media/ralewaymedium.272b676b.ttf"
+  },
+  {
+    "revision": "68b0f326ff690e24c2d009a978005c37",
+    "url": "/test/build/static/media/ralewaymedium.68b0f326.woff2"
+  },
+  {
+    "revision": "3dce8d235f50e3f8aa899e03046be9ce",
+    "url": "/test/build/static/media/ralewayregular.3dce8d23.woff2"
+  },
+  {
+    "revision": "6be3d3739d6d00d7cf6d0768e34563e1",
+    "url": "/test/build/static/media/ralewayregular.6be3d373.woff"
+  },
+  {
+    "revision": "8e7383647eac11f7ea3ab1538dee3c20",
+    "url": "/test/build/static/media/ralewayregular.8e738364.ttf"
+  },
+  {
+    "revision": "13965f458e6532423bd8aaf8729e9b42",
+    "url": "/test/build/static/media/ralewaysemibold.13965f45.woff2"
+  },
+  {
+    "revision": "5bb57688b573dda0812316fd2dc7fab8",
+    "url": "/test/build/static/media/ralewaysemibold.5bb57688.woff"
+  },
+  {
+    "revision": "8e21e18865d59fc69a201050244907b9",
+    "url": "/test/build/static/media/ralewaysemibold.8e21e188.ttf"
+  }
+]);
